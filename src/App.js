4ea4352a81f8de511e18546d459dad7487afc9c3
Bamdad Goshtasbi <bamdadgoshtasbi@gmail.com>
2023-11-08 13:18:10 -0800

    Developing Website for Coldplay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className ="whole-page" >
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Profile</li>
+          </ul>
+        </nav>  
+         <hr/>
       </header>
+      <div className="body">
+      <h1>The COLDPLAY</h1>
+        <h2> About</h2>
+        <p>
+        Coldplay are a British rock band formed in London in 1997. They consist of vocalist and pianist Chris Martin, guitarist Jonny Buckland, bassist Guy Berryman, drummer Will Champion and manager Phil Harvey.[a] They initially met at University College London, calling themselves Big Fat Noises and changing to Starfish, before settling on the current name. After independently releasing an extended play, Safety (1998), Coldplay signed with Parlophone in 1999. The band's debut album, Parachutes (2000), included their breakthrough single "Yellow" and received a Brit Award for British Album of the Year, a Grammy Award for Best Alternative Music Album and a Mercury Prize nomination. Their second album, A Rush of Blood to the Head (2002), won the same accolades and included the single "Clocks", which won a Grammy Award for Record of the Year. Their third album, X&Y (2005), which completed what the band considered a trilogy, was nominated for the Grammy Award for Best Rock Album. Their fourth album, Viva la Vida or Death and All His Friends (2008), won Best Rock Album. Both albums were the best-selling of their respective years, topping the charts in over 30 countries. Viva la Vida was also nominated for Album of the Year, while its title track became the first single by a British group to simultaneously reach number one in the United Kingdom and United States in the 21st century.
+        </p>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/ColdplayBBC071221_%28cropped%29.jpg/600px-ColdplayBBC071221_%28cropped%29.jpg"/>
+        <p>Chris Martin is the GOAT.
+        </p>
+      </div>
+      <hr/>
+      <div className="footer">
+        <h2>Listen to me</h2>
+        <ul>
+          <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ54R56WBMymhGb1IIc_f3zwbq-wFhQXNEjyT49Txvt&s"/></li>
+          <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/YouTube_icon_%282011-2013%29.svg/800px-YouTube_icon_%282011-2013%29.svg.png"/></li>
+          <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ54R56WBMymhGb1IIc_f3zwbq-wFhQXNEjyT49Txvt&s"/></li>
+        </ul>
+      </div>
+
     </div>
+
+
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,78 @@
+import './style.css'
+
+
+function Home() {
+  return (
+    <div className ="whole-page" >
+      <title>ColdPlay</title>
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Profile</li>
+          </ul>
+        </nav>  
+         <hr/>
+      </header>
+      <div className="body">
+        {/* Need a backgound image */}
+      <h1>The COLDPLAY</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/ColdplayBBC071221_%28cropped%29.jpg/600px-ColdplayBBC071221_%28cropped%29.jpg"/>
+        <button>Listen to our latest Album</button>
+        <button>&#9658;</button>
+        <h2>Coming Tours</h2>
+        <div className="tours">
+            <div>
+              <strong>July 16th SD</strong>
+              &lt;&gt;
+              SD theater 
+              &lt;&gt;
+              <button type="button">Buy your Ticket</button>
+              <hr/>
+            </div>
+            <div>
+              <strong>July 17th LA</strong>
+              &lt;&gt;
+              LA theater
+              &lt;&gt;
+              <button type="button">Buy your Ticket</button>
+              &lt;&gt;
+              <hr/>
+            </div>   
+            <div>
+              <strong>July 18th Washington</strong>
+              &lt;&gt;
+              Washington theater
+              &lt;&gt;
+              <button type="button">Buy your Ticket</button>
+              &lt;&gt;
+              <hr/>
+            </div>
+            <div>
+              <strong>July 19th Massachuset</strong>
+              &lt;&gt;
+              Massachuset theater
+              &lt;&gt;
+              <button type="button">Buy your Ticket</button>
+              &lt;&gt;
+            </div>                                 
+        </div>
+      </div>
+      <hr/>
+      <div className="footer">
+        <h2>Listen to me</h2>
+        <ul>
+          <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ54R56WBMymhGb1IIc_f3zwbq-wFhQXNEjyT49Txvt&s"/></li>
+          <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/YouTube_icon_%282011-2013%29.svg/800px-YouTube_icon_%282011-2013%29.svg.png"/></li>
+          <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ54R56WBMymhGb1IIc_f3zwbq-wFhQXNEjyT49Txvt&s"/></li>
+        </ul>
+      </div>
+
+    </div>
+
+
+  );
+}
+
+export default Home;
