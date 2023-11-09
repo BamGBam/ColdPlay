3cbd09faec75867b3ecd354174d27fbc3219a559
Bamdad Goshtasbi <bamdadgoshtasbi@gmail.com>
2023-11-09 11:20:05 -0800

    Adding more styling on style.css

--- src/App.js
+++ src/App.js
@@ -11,15 +11,13 @@ function App() {
             <li>About</li>
             <li>Profile</li>
           </ul>
+          <h1>The COLDPLAY</h1>
         </nav>  
-         <hr/>
       </header>
       
       <div className="body">
         <hr/>
         &gt;&lt;
-
-      <h1>The COLDPLAY</h1>
         <h2> About</h2>
         <p>
         Coldplay are a British rock band formed in London in 1997. They consist of vocalist and pianist Chris Martin, guitarist Jonny Buckland, bassist Guy Berryman, drummer Will Champion and manager Phil Harvey.[a] They initially met at University College London, calling themselves Big Fat Noises and changing to Starfish, before settling on the current name. After independently releasing an extended play, Safety (1998), Coldplay signed with Parlophone in 1999. The band's debut album, Parachutes (2000), included their breakthrough single "Yellow" and received a Brit Award for British Album of the Year, a Grammy Award for Best Alternative Music Album and a Mercury Prize nomination. Their second album, A Rush of Blood to the Head (2002), won the same accolades and included the single "Clocks", which won a Grammy Award for Record of the Year. Their third album, X&Y (2005), which completed what the band considered a trilogy, was nominated for the Grammy Award for Best Rock Album. Their fourth album, Viva la Vida or Death and All His Friends (2008), won Best Rock Album. Both albums were the best-selling of their respective years, topping the charts in over 30 countries. Viva la Vida was also nominated for Album of the Year, while its title track became the first single by a British group to simultaneously reach number one in the United Kingdom and United States in the 21st century.
